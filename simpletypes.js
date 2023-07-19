af4135e47e64a4757908a0212a874f318aac6dc0
Muhammad Usman AKbar <84783863+musmanfullstack@users.noreply.github.com>
2023-07-19 17:23:29 +0500

    types

--- simpletypes.js
+++ simpletypes.js
@@ -1,1 +1,28 @@
+/*
+Typescript simple types:
+
+TypeScript supports some simple types (primitives) you may know.
+
+There are three main primitives in JavaScript and TypeScript.
+boolean - true or false values
+number - whole numbers and floating point values
+string - text values like "TypeScript Rocks"
+
+There are also 2 less common primitives used in later versions of Javascript and TypeScript.
+bigint - whole numbers and floating point values, but allows larger negative and positive numbers than the number type.
+symbol are used to create a globally unique identifier.
+
+*/
 console.log("BISMILLAH");
+//Syntax of varaible initilization is: let variableName: dataType= value;
+var booleanVariable = true;
+var numberVariable = 9;
+var stringVariable = "here is string variable";
+console.log(booleanVariable);
+console.log(numberVariable);
+console.log(stringVariable);
+//explicit and implicit data types
+var nameofPerson = "Muhammad Usman";
+var nameofSecondPerson = "Sir";
+console.log(nameofPerson);
+console.log(nameofSecondPerson);
--- simpletypes.ts
+++ simpletypes.ts
@@ -1,1 +1,36 @@
+/*
+Typescript simple types:
+
+TypeScript supports some simple types (primitives) you may know.
+
+There are three main primitives in JavaScript and TypeScript.
+boolean - true or false values
+number - whole numbers and floating point values
+string - text values like "TypeScript Rocks"
+
+There are also 2 less common primitives used in later versions of Javascript and TypeScript.
+bigint - whole numbers and floating point values, but allows larger negative and positive numbers than the number type.
+symbol are used to create a globally unique identifier.
+
+*/
+
 console.log("BISMILLAH");
+//Syntax of varaible initilization is: let variableName: dataType= value;
+
+let booleanVariable: boolean=true;
+let numberVariable: number=9;
+let stringVariable: String="here is string variable";
+
+console.log(booleanVariable);
+console.log(numberVariable);
+console.log(stringVariable);
+
+
+//explicit and implicit data types
+
+let nameofPerson:String="Muhammad Usman";
+let nameofSecondPerson="Sir";
+
+console.log(nameofPerson);
+console.log(nameofSecondPerson);
+
